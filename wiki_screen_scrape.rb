bbb6db9f5c1177493e5d7f73f7737b60cbcd4214
Aravind Gopalakrishnan <pro.aravind@gmail.com>
2016-05-29 12:32:07 +0530

    added file data to the url

--- wiki_screen_scrape.rb
+++ wiki_screen_scrape.rb
@@ -1,24 +1,30 @@
 require "date"
 require "mechanize"
-mechanize = Mechanize.new
-page = mechanize.get('https://commons.wikimedia.org/wiki/Category:The_PDF_files_in_Tamil_without_OCR_conversion')
+mechanize = Mechanize.new do |a|
+  a.follow_meta_refresh = true
+  a.keep_alive = false
+end
+page = mechanize.get('https://commons.wikimedia.org/wiki/Category:PDF_files_in_Tamil_with_OCR_conversion')
 links = []
 page.search('#mw-category-media li .gallerytext a').each do |link|
-	links << link['href']
+  links << link['href']
 end
 host_name = page.uri.hostname
 scheme = page.uri.scheme
 
 File.open("url_#{DateTime.now.to_s}.txt",'w') do |f|
-	links.each do |link| 
-		link_with_base = "#{scheme}://#{host_name}#{link}"
-		# puts "Getting for...#{link_with_base}"
-		page = mechanize.get(link_with_base)
-		file_link = page.search('#mw-content-text .fullMedia a')[0]
-		output = "#{file_link['title']}~#{file_link['href']}"
-		puts output
-		f.puts output
-	end
+  links.each do |link|
+    link_with_base = "#{scheme}://#{host_name}#{link}"
+    # puts "Getting for...#{link_with_base}"
+    page = mechanize.get(link_with_base)
+    file_link = page.search('#mw-content-text .fullMedia a')[0]
+    file_data = page.search('#mw-content-text .fullMedia span.fileInfo')[0]
+    puts file_data.inspect
+    output = "#{file_link['title']}~#{file_link['href']}~#{file_data.text}"
+    puts output
+    # puts output
+    # f.puts output
+  end
 end
 
 puts "Completed...."
